660b0e12f756a5a879039ce103b6f776a0baf8fe
chlgkdms <haeun3882@gmail.com>
2023-07-04 22:38:57 +0900

    ⚡️ :: main activity 수정

--- app/src/main/java/com/example/hackathonproject/view/MainActivity.kt
+++ app/src/main/java/com/example/hackathonproject/view/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.example.hackathonproject
+package com.example.hackathonproject.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hackathonproject.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

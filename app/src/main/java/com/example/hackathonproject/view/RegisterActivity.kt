5719d9dbaa90f06fc3f7346630e6fae9ab2e6845
chlgkdms <haeun3882@gmail.com>
2023-07-04 22:39:17 +0900

    ⚡️ :: bottom navigation

--- app/src/main/java/com/example/hackathonproject/view/RegisterActivity.kt
+++ app/src/main/java/com/example/hackathonproject/view/RegisterActivity.kt
@@ -3,10 +3,13 @@ package com.example.hackathonproject.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hackathonproject.R
+import com.example.hackathonproject.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

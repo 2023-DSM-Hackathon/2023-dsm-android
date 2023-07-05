9395d4d49cdf5fa82a5da26e1032db47da705843
chlgkdms <haeun3882@gmail.com>
2023-07-05 22:48:09 +0900

    ⚡️ :: 작성 서버 연동

--- app/src/main/java/com/example/hackathonproject/view/SubmitActivity.kt
+++ app/src/main/java/com/example/hackathonproject/view/SubmitActivity.kt
@@ -3,10 +3,18 @@ package com.example.hackathonproject.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hackathonproject.R
+import com.example.hackathonproject.api.ApiProvider
+import com.example.hackathonproject.databinding.ActivitySubmitBinding
 
 class SubmitActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySubmitBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_submit)
+        binding = ActivitySubmitBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSubmit.setOnClickListener {
+            ApiProvider.retrofit
+        }
     }
 }

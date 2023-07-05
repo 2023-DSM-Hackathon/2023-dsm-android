f9990e6185c5ddbd2ba111d9ac2e900a2175ae60
chlgkdms <haeun3882@gmail.com>
2023-07-05 22:47:26 +0900

    ⚡️ :: 서버 연동 코드 수정

--- app/src/main/java/com/example/hackathonproject/api/ServerApi.kt
+++ app/src/main/java/com/example/hackathonproject/api/ServerApi.kt
@@ -1,24 +1,35 @@
 package com.example.hackathonproject.api
 
+import com.example.hackathonproject.model.LoginRequest
 import com.example.hackathonproject.model.LoginResponse
+import com.example.hackathonproject.model.RecruitResponse
 import com.example.hackathonproject.model.RegisterRequest
 import com.example.hackathonproject.model.UserInfoResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ServerApi {
     @POST("/users")
-    fun register() : Call<RegisterRequest>
+    fun register(
+        @Body registerRequest: RegisterRequest
+    ) : Call<Void>
 
     @POST("/users/token")
-    fun login() : Call<LoginResponse>
+    fun login(
+        @Body loginRequest: LoginRequest
+    ) : Call<LoginResponse>
 
     @GET("/users")
     fun getUserInfo(
         @Header("access_token") token: String
     ) : Call<UserInfoResponse>
+
+    @GET("/feeds")
+    fun getFeeds(
+    ) : Call<RecruitResponse>
 
 
 }
